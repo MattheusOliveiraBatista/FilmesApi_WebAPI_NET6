ce3d901daafb1fd46bb6fd16a681f723fed237a0
MattheusOliveiraBatista <matbatista.oliveira@gmail.com>
2023-02-13 10:03:45 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,46 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController: ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+       // if (!string.IsNullOrEmpty(filme.Titulo) && filme.Titulo.Length <= 500 &&
+      //      !string.IsNullOrEmpty(filme.Genero) &&
+       //     filme.Duracao >= 70)
+
+       
+            filme.Id = id++;
+
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+
+
+        
+
+    }
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return filmes;
+    }
+
+
+    //A interrogação siginifica que ele é nulo
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmes(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id== id);
+    }
+
+
+}
