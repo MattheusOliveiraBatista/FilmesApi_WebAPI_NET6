d715cca3238c54d2fdc45aa456c3c7431f53e8e8
matba <matba@DESKTOP-5AQAIDF>
2023-02-13 15:27:21 -0300

    Versão de hoje cedo no if

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -3,35 +3,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
 
+
+
+
 [ApiController]
 [Route("[controller]")]
-public class FilmeController: ControllerBase
+public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
     private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public void AdicionaFilme([FromBody] Filme filme )
     {
-       // if (!string.IsNullOrEmpty(filme.Titulo) && filme.Titulo.Length <= 500 &&
-      //      !string.IsNullOrEmpty(filme.Genero) &&
-       //     filme.Duracao >= 70)
-
-       
-            filme.Id = id++;
-
-            filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
+        // if (!string.IsNullOrEmpty(filme.Titulo) && filme.Titulo.Length <= 500 &&
+        //      !string.IsNullOrEmpty(filme.Genero) &&
+        //     filme.Duracao >= 70)
 
 
-        
+        filme.Id = id++;
+
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+
+
+
 
     }
     [HttpGet]
+    //public IEnumerable<Filme> RecuperaFilmes[FromQuery] int skip, int take)
+
     public IEnumerable<Filme> RecuperaFilmes()
     {
         return filmes;
+        //skip = quantos ele quer pular
+        //take = quantos ele quer pegar
+        //return filmes.Skip(skip).Take(take);
     }
 
 
@@ -39,8 +47,7 @@ public class FilmeController: ControllerBase
     [HttpGet("{id}")]
     public Filme? RecuperaFilmes(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id== id);
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
-
 
 }
