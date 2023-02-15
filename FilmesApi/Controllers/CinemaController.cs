e965d45ffe15585fa179a57d06342708edf3e109
matba <matba@DESKTOP-5AQAIDF>
2023-02-15 19:45:24 -0300

    FilmeComCinema

---
+++ FilmesApi/Controllers/CinemaController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CinemaController: ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public  CinemaController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AdicionaCinema([FromBody] CreateCinemaDto cinemaDto)
+        {
+            Cinema cienema = _mapper.Map<Cinema>(cinemaDto);
+
+        }
+    }   
+}
